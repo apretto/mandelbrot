535d2a6400d0505b06165e4d7a724f77c92a6399
Alexandre Pretto Nunes <anunes@thoughtworks.com>
2015-04-30 18:14:55 -0300

    Better handle different views and sizes, and improve coloring

--- mandelbrot.js
+++ mandelbrot.js
@@ -1,12 +1,14 @@
 var plot = document.querySelector("#plot");
 
-plot.height = 600;
-plot.width = 600;
+plot.height = 800;
+plot.width = 800;
+
+var totalPixels = plot.height * plot.width;
 
 var view = {
   left: -2,
   up: 2,
-  right: 2,
+  right: 1,
   down: -2
 };
 
@@ -16,8 +18,8 @@ var step = {
 };
 
 function offsetToComplex (offset) {
-  var x = offset % 600;
-  var y = math.floor(offset / 600);
+  var x = offset % plot.width;
+  var y = math.floor(offset / plot.width);
 
   return math.complex({
     re: x * step.re + view.left,
@@ -36,7 +38,7 @@ function generateSpace() {
 
       return current;
     };
-  }(), 600*600);
+  }(), totalPixels);
 }
 
 var points = generateSpace();
@@ -45,37 +47,48 @@ var image = plot.getContext('2d').createImageData(plot.width, plot.height);
 
 var goesToInfinity = function (complexNumber, iterations) {
   var c = complexNumber;
+  var minRadius = 10;
 
-  function isOutsideRadius2(complexNumber) {
-    return (math.pow(complexNumber.re, 2) + math.pow(complexNumber.im, 2)) > 4;
+  function radius(complexNumber) {
+    return math.pow(complexNumber.re, 2) + math.pow(complexNumber.im, 2);
   }
 
   function iterate(complexNumber, currentIteration) {
-    if (isOutsideRadius2(complexNumber)) {
-      return currentIteration;
+    var currentRadius = radius(complexNumber);
+    minRadius = currentRadius < minRadius ? currentRadius : minRadius;
+
+    if (currentRadius > 4) {
+      return {iterations: currentIteration, minRadius: minRadius};
     }
 
     if (currentIteration < iterations) {
       return iterate(math.add(math.pow(complexNumber, 2), c), currentIteration + 1);
     }
 
-    return currentIteration;
+    return {iterations: -1, minRadius: minRadius};
   }
 
   return iterate(complexNumber, 1);
 };
 
 var colormap = {
-  0: [0, 0, 0, 255],
-  1: [255, 0, 0, 255],
-  2: [0, 255, 0, 255],
-  3: [0, 0, 255, 255]
+  '-1': [0, 0, 0],
+  0: [0, 255, 255],
+  1: [0, 255, 0],
+  2: [0, 255, 255],
+  3: [0, 0, 255]
 };
 
 var i = 0;
 points.map(
   function(point) {
-    return colormap[goesToInfinity(point, 20) % 4];
+    var pointInfo = goesToInfinity(point, 100);
+    //return [0, 0, 255 - (pointInfo.minRadius / 2 * 255), 255];
+    //return colormap[math.floor(pointInfo.minRadius * 10) % 4].concat(255);
+    var color = colormap[pointInfo.iterations % 4];
+    return Lazy(color).map(function (component) {
+      return component - (pointInfo.minRadius / 4 * 128);
+    }).toArray().concat(255);
   })
   .flatten()
   .each(function (pixelComponent){
